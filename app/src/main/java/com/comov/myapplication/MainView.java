ca63eee90d863263708d6fbefcb3cd89ee24bfa4
antonioponce21 <tony_pz-97@hotmail.com>
2020-05-12 19:40:27 +0200

    RecyclerView para canales

--- app/src/main/java/com/comov/myapplication/MainView.java
+++ app/src/main/java/com/comov/myapplication/MainView.java
@@ -7,11 +7,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.comov.myapplication.adapters.ChannelAdapter;
 import com.comov.myapplication.apiTools.APIUtils;
+
 import com.comov.myapplication.datamodel.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,7 +25,9 @@ import retrofit2.Response;
 
 public class MainView extends AppCompatActivity {
     private com.comov.myapplication.apiTools.APIService APIService;
-    //private ImageView image;
+    List<Channel> channels;
+    List<Channel> prueba;
+    RecyclerView recyclerViewChannel;
     //private EditText message;
     //private Button btnSend;
 
@@ -33,35 +41,48 @@ public class MainView extends AppCompatActivity {
         TextView nameTxt = findViewById(R.id.user_name);
         String finalTxt = "Hi "+username;
         nameTxt.setText(finalTxt);
-
+        channels = new ArrayList<Channel>();
         //Get Channels
         getChannelsFromUser(username);
+
+        //Init recycler
+        prueba = new ArrayList<Channel>();
+        for (int i=0;i<10;i++){
+            prueba.add( new Channel("Channel " +i,null,"wtf"));
+        }
+
+        System.out.println("Peta 1");
+        recyclerViewChannel = findViewById(R.id.channelsList);
+        System.out.println("Peta 2");
+        ChannelAdapter channelAdapter = new ChannelAdapter(prueba);
+        System.out.println("Peta 3");
+        recyclerViewChannel.setAdapter(channelAdapter);
+        System.out.println("Peta 4");
+        recyclerViewChannel.setLayoutManager(new LinearLayoutManager(this));
+        System.out.println("Por que no pinta?");
+
     }
 
     public void getChannelsFromUser(String user){
+
         APIService.getChannel(user).enqueue(new Callback<ChannelResponse>(){
             @Override
             public void onResponse(Call<ChannelResponse> call, Response<ChannelResponse> response) {
                 if (response.code() == 200) {
                     Toast.makeText(getApplicationContext(), "Got chats", Toast.LENGTH_LONG).show();
-                    /*System.out.println(response.body() + " \n#############################" );
-                    JsonArray channels = (JsonArray) response.body().getAsJsonArray("ChannelsObj");
-                    System.out.println(channels.get(0)+ " \n" + channels.get(1)+ " \n" +channels.get(2)+ " \n#############################" );
-                    System.out.println(channels.size()+ "  SIZE  \n#############################" );
-                    //for ( int i < channels.size() )
-                    JsonObject aux = channels.get(0).getAsJsonObject();
-                    System.out.println(aux.get("uid") + " -> uid\n" + aux.get("title") + " -> title\n" + aux.get("usuarios") + " -> usuarios\n#############################");
-                    */
-                    List<Channel> channels = response.body().getChannels();
+                    channels = response.body().getChannels();
                     channels.get(0);
                     System.out.println(channels.get(0).getTitle() + " \n#############################");
                     System.out.println(response.toString()+ " \n#############################");
+
                 } else if (response.code() == 404 )
                     Toast.makeText(getApplicationContext(), "Chats not found. Tu princesa esta en otro castillo." +
                             "", Toast.LENGTH_LONG).show();
             }
             @Override
+
             public void onFailure(Call<ChannelResponse> call, Throwable t){
+
                 Toast.makeText(getApplicationContext(), "Fail "+ t, Toast.LENGTH_LONG).show();
                 System.out.println(t + " #######################################################################");
             }
--- app/src/main/java/com/comov/myapplication/adapters/ChannelAdapter.java
+++ app/src/main/java/com/comov/myapplication/adapters/ChannelAdapter.java
@@ -1,34 +1,68 @@
 package com.comov.myapplication.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.comov.myapplication.R;
 import com.comov.myapplication.datamodel.Channel;
 import com.comov.myapplication.datamodel.Users;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ChannelAdapter extends RecyclerView.Adapter {
-    Users current_user;
+public class ChannelAdapter extends RecyclerView.Adapter<ChannelAdapter.ViewHolderChannel>{
     ArrayList<Channel> channels;
+
+    public ChannelAdapter(List<Channel> channels){
+        this.channels = new ArrayList<Channel>();
+        this.channels.addAll(channels);
+
+    }
+
+    public class ViewHolderChannel extends RecyclerView.ViewHolder{
+
+        TextView title;
+
+        public ViewHolderChannel(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.titleChannel);
+        }
+
+        public TextView getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title.setText(title);
+        }
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolderChannel onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view =  LayoutInflater.from(parent.getContext()).inflate(R.layout.item_channel,parent,false);
+        return new ViewHolderChannel(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolderChannel holder, int position) {
+        Channel channel = channels.get(position);
+        holder.setTitle(channel.getTitle());
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return channels.size();
     }
+
 
 
 
